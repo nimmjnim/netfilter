d0de9d506f9eee08462add8cd51312996bd35230
nimmjnim <52564524+nimmjnim@users.noreply.github.com>
2019-08-27 01:41:15 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>		/* for NF_ACCEPT */
+#include <errno.h>
+#include <string.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+char Xdomain[256];
+
+/* returns packet id */
+
+static u_int32_t print_pkt (struct nfq_data *tb, int *flag)
+{
+	int id = 0;
+	struct nfqnl_msg_packet_hdr *ph;
+	struct nfqnl_msg_packet_hw *hwph;
+	u_int32_t mark,ifi; 
+	int ret;
+	unsigned char *data;
+	uint8_t ip_hl, th_off;  
+	uint16_t len;  
+	unsigned char *http;
+
+	ph = nfq_get_msg_packet_hdr(tb);
+	if (ph) id = ntohl(ph->packet_id);
+	hwph = nfq_get_packet_hw(tb);	
+	ret = nfq_get_payload(tb, &data);
+		
+	*flag = 0;
+	if(data[9] == 0x06){
+		ip_hl = ((data[0] << 4) & 0xff) >> 4;	
+		len = ret;	
+		th_off = data[ip_hl*4+12] >> 4;
+		http = data+(ip_hl*4+th_off*4);
+		if(!memcmp(http, "GET", 3)){
+			if(memcmp(http+22, Xdomain, strlen(Xdomain)) == 0){
+				 printf("[*] Cannot access %s\n", Xdomain);
+				 *flag = 1;
+				 }
+			else *flag = 0;
+		} 		
+	}
+	return id;
+}
+	
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+	      struct nfq_data *nfa, void *data)
+{
+	int flag = 0;
+	u_int32_t id = print_pkt(nfa, &flag);
+	if(flag == 1) return nfq_set_verdict(qh, id, NF_DROP, 0, NULL);
+	else return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
+}
+
+int main(int argc, char **argv)
+{
+	struct nfq_handle *h;
+	struct nfq_q_handle *qh;
+	struct nfnl_handle *nh;
+	int fd;
+	int rv;
+	char buf[4096] __attribute__ ((aligned));
+
+	printf("opening library handle\n");
+	h = nfq_open();
+	if (!h) {
+		fprintf(stderr, "error during nfq_open()\n");
+		exit(1);
+	}
+
+	printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+	if (nfq_unbind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_unbind_pf()\n");
+		exit(1);
+	}
+
+	printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+	if (nfq_bind_pf(h, AF_INET) < 0) {
+		fprintf(stderr, "error during nfq_bind_pf()\n");
+		exit(1);
+	}
+
+	printf("binding this socket to queue '0'\n");
+	qh = nfq_create_queue(h,  0, &cb, NULL);
+	if (!qh) {
+		fprintf(stderr, "error during nfq_create_queue()\n");
+		exit(1);
+	}
+
+	printf("setting copy_packet mode\n");
+	if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+		fprintf(stderr, "can't set packet_copy mode\n");
+		exit(1);
+	}
+	
+	strncpy(Xdomain, argv[1], strlen(argv[1]));
+	fd = nfq_fd(h);
+
+	for (;;) {
+		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+			nfq_handle_packet(h, buf, rv);
+			continue;
+		}
+		/* if your application is too slow to digest the packets that
+		 * are sent from kernel-space, the socket buffer that we use
+		 * to enqueue packets may fill up returning ENOBUFS. Depending
+		 * on your application, this error may be ignored. nfq_nlmsg_verdict_putPlease, see
+		 * the doxygen documentation of this library on how to improve
+		 * this situation.
+		 */
+		if (rv < 0 && errno == ENOBUFS) {
+			printf("losing packets!\n");
+			continue;
+		}
+		perror("recv failed");
+		break;
+	}
+
+	printf("unbinding from queue 0\n");
+	nfq_destroy_queue(qh);
+
+#ifdef INSANE
+	/* normally, applications SHOULD NOT issue this command, since
+	 * it detaches other programs/sockets from AF_INET, too ! */
+	printf("unbinding from AF_INET\n");
+	nfq_unbind_pf(h, AF_INET);
+#endif
+
+	printf("closing library handle\n");
+	nfq_close(h);
+
+	exit(0);
+}
+
